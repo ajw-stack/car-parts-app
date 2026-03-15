e7434c076b900ed02178792a964150e9d3f2bee6
ajw-stack <ajwindsor86@gmail.com>
2026-03-15 17:47:41 +1100

    fav sharper

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,13 @@ export const metadata: Metadata = {
   title: "Global Parts Catalogue",
   description: "Global Parts Catalogue",
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
+    ],
+    apple: "/apple-touch-icon.png"
+  }
 };
 
 export default function RootLayout({
